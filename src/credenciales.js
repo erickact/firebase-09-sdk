897842671853c6009fd936d44f87f458a004b51a
erickact <castillotacuriericka@gmail.com>
2021-11-23 14:13:42 -0500

    firebase 9 (auntenticacion+firestore+storage) con react

---
+++ src/credenciales.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwfdOR0JSwZZkwjVQgVxBgh0dZQ6JFThI",
+  authDomain: "fir-09-sdk.firebaseapp.com",
+  projectId: "fir-09-sdk",
+  storageBucket: "fir-09-sdk.appspot.com",
+  messagingSenderId: "309846044050",
+  appId: "1:309846044050:web:11e1fdcabefb1573b2254b",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
